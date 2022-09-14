26b2f2818c91f7c5b367c6eaaebe0f4e07b9d1cb
F0lkl0re <51024076+F0lkl0re@users.noreply.github.com>
2022-09-14 10:24:52 -0400

    Fixed calculations and data types

--- src/main.rs
+++ src/main.rs
@@ -8,22 +8,23 @@ fn main() {
     .expect("Failed to read line");
     let _numberassignments: i32 = numberassignments.trim().parse().expect("Please type a number!");
     let mut i = 0;
-    let mut totalgrade = 0;
-    let mut totalweight = 0;
+    let mut totalgrade = 0.0;
+    let mut totalweight = 0.0;
     while i < _numberassignments{
         println!("What is your grade for this assignment? ({})", i+1);
         let mut grade = String::new();
         io::stdin()
         .read_line(&mut grade)
         .expect("Failed to read line");
-        let grade: i32 = grade.trim().parse().expect("Please input a number");
+        let grade: f32 = grade.trim().parse().expect("Please input a number");
         println!("What is the weight for this assignment ({})", i+1);
         let mut weight = String::new();
         io::stdin()
         .read_line(&mut weight)
         .expect("Failed to read line");
-        let weight: i32 = weight.trim().parse().expect("Please input a number");
-        totalgrade = totalgrade + grade;
+        let weight: f32 = weight.trim().parse().expect("Please input a number");
+        let gweight = weight * grade;
+        totalgrade = totalgrade + gweight;
         totalweight = totalweight + weight;
         i = i + 1;
     }
